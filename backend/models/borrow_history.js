9594e4c3ab58e8c3f9387f686fd4de1996dd714c
codeweb404 <codeweb.jcu@gmail.com>
2024-09-09 23:46:41 +0900

    committed files not included in last commit

--- backend/models/borrow_history.js
+++ backend/models/borrow_history.js
@@ -3,5 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const borrowHistorySchema = new Schema({
-    item: {type: Schema.Types.ObjectId, }
+    item: {type: Schema.Types.ObjectId, ref: "Item", required: true},
+    quantity: {type: Number, required: true},
+    borrower: {type: Schema.Types.ObjectId, ref: "Team", required: true},
+    borrow_date: {type: Date, required: true},
+    return_date: {type: Date, required: true},
 })
