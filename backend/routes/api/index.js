411c79b628654bb919f6af0ec427038d97178288
codeweb404 <codeweb.jcu@gmail.com>
2024-09-11 17:28:05 +0900

    connected api routers to api/index

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,4 +6,18 @@ router.get('/', (req, res) => {
   res.json({ message: 'API Index' });
 });
 
+const itemRouter = require("./item");
+const teamRouter = require("./team");
+const tagRouter = require("./tag");
+const categoryRouter = require("./category");
+const borrowHistoryRouter = require("./borrow_history");
+const adminRouter = require("./admin");
+
+router.use("/item", itemRouter);
+router.use("/team", teamRouter);
+router.use("/tag", tagRouter);
+router.use("/category", categoryRouter);
+router.use("/borrow-history", borrowHistoryRouter);
+router.use("/adminRouter", adminRouter);
+
 module.exports = router;
