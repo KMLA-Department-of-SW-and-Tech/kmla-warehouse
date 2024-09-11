9244d3db94eaf749d2c0a23cc7016617f77bbc18
codeweb404 <codeweb.jcu@gmail.com>
2024-09-11 16:27:08 +0900

    separated quantity into total quantity(the total number of items) and available quantity(the number of items which are availiable) in item

--- backend/models/item.js
+++ backend/models/item.js
@@ -6,7 +6,8 @@ const itemSchema = new Schema({
     name: {type: String, required: true, maxLength: 100},
     description: {type: String, maxLength: 1000},
     tags: {type: [Schema.Types.ObjectId], ref: "Tag"},
-    quantity: {type: Number, required: true},
+    total_quantity: {type: Number, required: true},
+    available_quantity: {type: Number, required: true},
     location: {type: String, maxLength: 100},
     // photo will be added later
     category: {type: Schema.Types.ObjectId, ref: "Category"},
