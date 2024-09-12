2c919e5c32353549193cbb7a53ff6ced4f004278
Yean Lee <talk2yeanlee@gmail.com>
2024-09-12 10:27:33 +0900

    added equipment-home page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import axios from 'axios';
 
-import Login from "./non-authenticated/login/page.tsx"; 
-import Signup from "./non-authenticated/signup/page.tsx"; 
+// import Login from "./non-authenticated/login/page.tsx"; 
+// import Signup from "./non-authenticated/signup/page.tsx"; 
 
 import Home from "./(authenticated)/equipment-home/equipment-home.tsx"; 
-import EquipmentDetails from "./(authenticated)/equipment-home/equipment-details.tsx"; 
-import FavoritePage from "./(authenticated)/user-home/favorite-page.tsx"; 
-import ReservationStatus from "./(authenticated)/user-home/reservation-status.tsx"
-import UserHistoryPage from "./(authenticated)/user-home/history-page.tsx"; 
-
-import AddEquipmentPage from "./admin/add-equipment-page.tsx"; 
-import AdminHistoryPage from "./admin/history-page.tsx";  
+// import EquipmentDetails from "./(authenticated)/equipment-home/equipment-details.tsx"; 
+// import FavoritePage from "./(authenticated)/user-home/favorite-page.tsx"; 
+// import ReservationStatus from "./(authenticated)/user-home/reservation-status.tsx"
+// import UserHistoryPage from "./(authenticated)/user-home/history-page.tsx"; 
+// 
+// import AddEquipmentPage from "./admin/add-equipment-page.tsx"; 
+// import AdminHistoryPage from "./admin/history-page.tsx";  
 
 
 const router = createBrowserRouter([
@@ -21,45 +21,45 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   // Equipment details page
-  {
-    path: "/kmla-warehouse/item/:itemId",
-    element: <EquipmentDetails />,
-  },
-  // Favorite page
-  {
-    path: "/kmla-warehouse/favorites",
-    element: <FavoritePage />,
-  },
-  // Reservation Status page
-  {
-    path: "/kmla-warehouse/reservation-status",
-    element: <ReservationStatus />,
-  },
-  // User rental history page
-  {
-    path: "/kmla-warehouse/rental-history",
-    element: <UserHistoryPage />,
-  },
-  // Login page
-  {
-    path: "/kmla-warehouse/login",
-    element: <Login />,
-  },
-  // Signup page
-  {
-    path: "/kmla-warehouse/signup",
-    element: <Signup />,
-  },
-  // Admin: add equipment page
-  {
-    path: "/kmla-warehouse/admin/upload-item",
-    element: <AddEquipmentPage />,
-  },
-  // Admin: history page
-  {
-    path: "/kmla-warehouse/admin/history",
-    element: <AdminHistoryPage />,
-  },
+  //{
+  //  path: "/kmla-warehouse/item/:itemId",
+  //  element: <EquipmentDetails />,
+  //},
+  //// Favorite page
+  //{
+  //  path: "/kmla-warehouse/favorites",
+  //  element: <FavoritePage />,
+  //},
+  //// Reservation Status page
+  //{
+  //  path: "/kmla-warehouse/reservation-status",
+  //  element: <ReservationStatus />,
+  //},
+  //// User rental history page
+  //{
+  //  path: "/kmla-warehouse/rental-history",
+  //  element: <UserHistoryPage />,
+  //},
+  //// Login page
+  //{
+  //  path: "/kmla-warehouse/login",
+  //  element: <Login />,
+  //},
+  //// Signup page
+  //{
+  //  path: "/kmla-warehouse/signup",
+  //  element: <Signup />,
+  //},
+  //// Admin: add equipment page
+  //{
+  //  path: "/kmla-warehouse/admin/upload-item",
+  //  element: <AddEquipmentPage />,
+  //},
+  //// Admin: history page
+  //{
+  //  path: "/kmla-warehouse/admin/history",
+  //  element: <AdminHistoryPage />,
+  //},
 ]);
 
 const App = () => {
