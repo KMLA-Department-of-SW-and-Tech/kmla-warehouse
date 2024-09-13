b6801a5c11ccf9dc544f893e72a542de8f2987c9
Yean Lee <talk2yeanlee@gmail.com>
2024-09-13 13:04:56 +0900

    made login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 //import axios from 'axios';
 
-// import Login from "./non-authenticated/login/page.tsx"; 
+import Login from "./non-authenticated/login/page.tsx"; 
 // import Signup from "./non-authenticated/signup/page.tsx"; 
 
 import Home from "./(authenticated)/equipment-home/equipment-home.tsx"; 
-// import EquipmentDetails from "./(authenticated)/equipment-home/equipment-details.tsx"; 
+import EquipmentDetails from "./(authenticated)/equipment-home/equipment-details.tsx"; 
 // import FavoritePage from "./(authenticated)/user-home/favorite-page.tsx"; 
 // import ReservationStatus from "./(authenticated)/user-home/reservation-status.tsx"
 // import UserHistoryPage from "./(authenticated)/user-home/history-page.tsx"; 
