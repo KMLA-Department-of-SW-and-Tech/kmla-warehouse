d32d5e2b87908dd6ad114989d08ee671c622bddc
codeweb404 <codeweb.jcu@gmail.com>
2024-10-01 20:10:46 +0900

    implemented delete for item

--- backend/routes/api/item.js
+++ backend/routes/api/item.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const verifyJWT = require("../../middleware/verifyJWT");
 const verifyRoles = require("../../middleware/verifyRoles"); // in case of admin secrurity
+const deleteItem = require("../../middleware/delete_item");
 
 const itemController = require("../../controllers/item_controller");
+
 
 router.get("/list", verifyJWT, itemController.item_list);
 
@@ -14,7 +16,7 @@ router.route("/:id")
     .get(verifyJWT, itemController.item_detail)
     .put(verifyJWT, itemController.item_update_put)
     .patch(verifyJWT, itemController.item_update_patch)
-    .delete(verifyJWT, itemController.item_delete);
+    .delete(verifyJWT, deleteItem, itemController.item_update_put);
 
 router.route("/:id/borrow")
     .post(verifyJWT, itemController.item_borrow);
--- frontend/src/(non-authenticated)/login/page.tsx
+++ frontend/src/(non-authenticated)/login/page.tsx
@@ -20,7 +20,7 @@ const LoginPage = () => {
       try { // example of axiosPrivate usage
         const response = await axiosPrivate.get("/api/auth/");
         console.log(response);
-        navigate("/kmla-warehouse/home"); //살려라!!!
+        // navigate("/kmla-warehouse/home"); //살려라!!!
       } catch (err) {
         console.log(err);
       }
@@ -74,11 +74,15 @@ const LoginPage = () => {
           await authService.logout();
         }
       }>Logout</button>
-      {/* <button onClick={
+      <button onClick={
         async () => {
-          await axiosPrivate.get('/api/team/66f5e847eccf4b831081cfcd/borrow-list');
+          try {
+            await axiosPrivate.delete('/api/item/66f8218b5743d574b5f90936');
+          } catch (err) {
+            console.error(err.response.data);
+          }
         }
-      }>Click Me</button> */}
+      }>Click Me</button>
       
         </div>
       </div>
