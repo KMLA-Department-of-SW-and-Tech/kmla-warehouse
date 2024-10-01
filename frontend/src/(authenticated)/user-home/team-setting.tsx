1fe2ae649c214e4180c20c9c0185ffa05ab833a9
Yean Lee <talk2yeanlee@gmail.com>
2024-10-01 09:45:24 +0900

    login page changed

---
+++ frontend/src/(authenticated)/user-home/team-setting.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import { teamService, Team } from '../../api/teamService';
+import { PageLayout } from '../../layouts/page-layout'; 
+import { useParams } from 'react-router-dom';
+import { Typography, Spin, Layout, Button } from 'antd';
+
+const TeamSettingPage: React.FC = () => {
+  const { id } = useParams<{ id: string }>(); // URL에서 팀 ID를 가져옴
+  const [teamName, setTeamName] = useState('');
+  const [nickname, setNickname] = useState(''); // 이게 팀 설정에 필요한지는 상황에 따라 다름
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [team, setTeam] = useState<Team | null>(null);
+
+  useEffect(() => {
+    const fetchTeamInfo = async () => {
+      setIsLoading(true);
+      try {
+        const teamData = await teamService.getTeamInfo(Number(id));
+        setTeam(teamData);
+        setTeamName(teamData.name);
+        // 필요한 경우 다른 필드도 설정
+      } catch (error) {
+        setError('팀 정보를 불러오는 중 문제가 발생했습니다.');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (id) {
+      fetchTeamInfo();
+    }
+  }, [id]);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await teamService.updateTeam(Number(id), { name: teamName });
+      alert('팀 정보가 성공적으로 수정되었습니다.');
+    } catch (error) {
+      setError('팀 정보를 수정하는 중 문제가 발생했습니다.');
+    }
+  };
+
+  if (isLoading) return <div>로딩 중...</div>;
+  if (error) return <div>{error}</div>;
+
+  return (
+    <PageLayout layout="narrow" isCentered={true}>
+      <div className="bg-white rounded-xl shadow-lg p-10 w-full">
+        <h2 className="text-center text-4xl font-bold text-gray-900 mb-8">팀 설정</h2>
+        {team ? (
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div className="space-y-4">
+              <div>
+                <input
+                  id="team-name"
+                  name="team-name"
+                  type="text"
+                  required
+                  className="w-full border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-black placeholder-gray-500 text-gray-900 sm:text-lg"
+                  placeholder="팀 이름"
+                  value={teamName}
+                  onChange={(e) => setTeamName(e.target.value)}
+                />
+              </div>
+              {/* 추가 필드가 필요하다면 여기 추가 */}
+            </div>
+
+            <div>
+              <Button
+                className="w-full py-3 text-lg font-medium text-white bg-black rounded-lg hover:bg-gray-800 focus:outline-none"
+              >
+                팀 정보 수정
+              </Button>
+            </div>
+          </form>
+        ) : (
+          <div>팀 정보를 불러올 수 없습니다.</div>
+        )}
+      </div>
+    </PageLayout>
+  );
+};
+
+export default TeamSettingPage;
--- frontend/src/(non-authenticated)/login/page.tsx
+++ frontend/src/(non-authenticated)/login/page.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import React from "react";
 import axiosPrivate from "../../hooks/axiosPrivate";
 import authService from "../../api/authService";
+import './page.css';
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
@@ -28,8 +29,46 @@ const LoginPage = () => {
     }
   }
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <button onClick={
+    <div className="login-container">
+      <div className="login-sidebar">
+        <div className="login-box">
+          <h2 className="login-title">로그인</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="username" className="form-label">
+                아이디
+              </label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="form-input"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password" className="form-label">
+                비밀번호
+              </label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="form-input"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="submit-button"
+            >
+              로그인
+            </button>
+
+          </form>
+          <button onClick={
         async () => {
           await authService.logout();
         }
@@ -39,46 +78,12 @@ const LoginPage = () => {
           await axiosPrivate.get('/api/item/list');
         }
       }>Click Me</button> */}
-      <div className="p-8 bg-white rounded shadow-md w-96">
-        <h2 className="mb-6 text-2xl font-bold text-center">로그인</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="username" className="block mb-2 text-sm font-bold">
-              아이디
-            </label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="w-full px-3 py-2 border rounded"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label htmlFor="password" className="block mb-2 text-sm font-bold">
-              비밀번호
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-3 py-2 border rounded"
-              required
-            />
-          </div>
-          {error ? <div>{error}</div> : <></>}
-          <button
-            type="submit"
-            className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-600"
-          >
-            로그인
-          </button>
-        </form>
+        </div>
       </div>
     </div>
   );
 };
 
-export default LoginPage;
+export default LoginPage; 
+
+
