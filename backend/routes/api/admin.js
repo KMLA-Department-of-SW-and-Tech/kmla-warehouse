440313345da0fa4811510fd3f99653580c44a955
hyk03031 <yha525320@gmail.com>
2024-10-02 16:54:39 +0900

    fixing editable table

--- backend/routes/api/admin.js
+++ backend/routes/api/admin.js
@@ -5,20 +5,11 @@ const verifyJWT = require("../../middleware/verifyJWT");
 const adminController = require("../../controllers/admin_controller");
 
 
-<<<<<<< HEAD
-router.post("/", verifyJWT,adminController.admin_create);
-
-router.route("/:id")
-    .get(verifyJWT,adminController.admin_detail)
-    .put(verifyJWT,adminController.admin_update_put)
-    .delete(verifyJWT,adminController.admin_delete);
-=======
 router.post("/", verifyJWT, adminController.admin_create);
 
 router.route("/:id")
     .get(verifyJWT, adminController.admin_detail)
     .put(verifyJWT, adminController.admin_update_put)
     .delete(verifyJWT, adminController.admin_delete);
->>>>>>> 72f11e50351c767534e681205f1af04201d73ba3
 
 module.exports = router;
