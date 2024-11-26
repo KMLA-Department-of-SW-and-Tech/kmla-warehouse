431b5a8df89ed0d74414f6c17f736ed243e79755
Yean Lee <talk2yeanlee@gmail.com>
2024-11-26 19:16:15 +0900

    back to home by clicking logo

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,28 +1,38 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // 페이지 이동을 위한 훅
 import './header.css';
-import currentUser from '../api/authService'
+import currentUser from '../api/authService';
 
 // Header 컴포넌트 정의
 const Headbar: React.FC = () => {
   const [currentUserName, setCurrentUserName] = useState<string | null>(null);
-  useEffect( () => {
+  const navigate = useNavigate(); // 페이지 이동을 위한 navigate 함수 생성
+
+  useEffect(() => {
     const fetchCurrentUser = async () => {
       const data = await currentUser.currentUser();
       setCurrentUserName(data);
     };
-  fetchCurrentUser();
-  }, [] )
+    fetchCurrentUser();
+  }, []);
+
+  const handleLogoClick = () => {
+    navigate('/kmla-warehouse/home'); // 로고 클릭 시 홈 화면으로 이동
+  };
+
   return (
     <header className="head">
       <div className="head-container">
-        <div className="logo">
+        
+        <div className="logo" onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
           KMLA Warehouse
         </div>
         <div className="user-info">
-          <span>{currentUserName ? currentUserName : ""}</span> 
+          <span>{currentUserName ? currentUserName : ''}</span>
         </div>
       </div>
     </header>
   );
 };
+
 export default Headbar;
