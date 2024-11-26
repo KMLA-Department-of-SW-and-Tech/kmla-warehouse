66b5448255b4bfd46d606075cbacc77f02bb932f
Yean Lee <talk2yeanlee@gmail.com>
2024-11-26 19:47:44 +0900

    user info text at header changed

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -10,8 +10,13 @@ const Headbar: React.FC = () => {
 
   useEffect(() => {
     const fetchCurrentUser = async () => {
-      const data = await currentUser.currentUser();
-      setCurrentUserName(data);
+      try {
+        const data = await currentUser.currentUser();
+        setCurrentUserName(data);
+      } catch (error) {
+        console.error('Error fetching user info:', error);
+        setCurrentUserName(null); // 에러 발생 시 로그인 상태를 비로그인으로 처리
+      }
     };
     fetchCurrentUser();
   }, []);
@@ -23,12 +28,15 @@ const Headbar: React.FC = () => {
   return (
     <header className="head">
       <div className="head-container">
-        
         <div className="logo" onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
           KMLA Warehouse
         </div>
         <div className="user-info">
-          <span>{currentUserName ? currentUserName : ''}</span>
+          {currentUserName ? (
+            <span>{currentUserName}님, KMLA WAREHOUSE에 오신 것을 환영합니다</span>
+          ) : (
+            <span>로그인을 하신 후 다른 기능을 사용하실 수 있습니다</span>
+          )}
         </div>
       </div>
     </header>
