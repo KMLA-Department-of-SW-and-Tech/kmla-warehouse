438eaad1af22d2c18e03075e8a90e79b630b022f
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-12-26 01:03:09 +0900

    manually parse req.body if Buffer comes

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const querystring = require('querystring');
 require('dotenv').config();
 
 
@@ -28,13 +29,30 @@ async function main() {
   await mongoose.connect(mongoDB);
 }
 
-// view engine setup
-app.use((req, res, next) => {
-  console.log("check",Buffer.isBuffer(req.body))
-  next();
-})
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// req.body가 Buffer인 경우 이를 JSON으로 파싱하는 미들웨어
+app.use((req, res, next) => {
+  if (Buffer.isBuffer(req.body) && req.is('application/json')) {
+    try {
+      req.body = JSON.parse(req.body.toString());
+    } catch (error) {
+      return res.status(400).send('Invalid JSON');
+    }
+  }
+  next();
+});
+
+// req.body가 Buffer인 경우 이를 URL-encoded 데이터로 파싱하는 미들웨어
+app.use((req, res, next) => {
+  if (Buffer.isBuffer(req.body) && req.is('application/x-www-form-urlencoded')) {
+    const bodyString = req.body.toString('utf-8');
+    req.body = querystring.parse(bodyString);
+  }
+  next();
+});
+
 app.use(logger(env === 'development'? 'dev' : 'combined'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
