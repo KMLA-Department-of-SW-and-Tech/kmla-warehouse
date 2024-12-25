f8a785264b98f766e5058993c7fa2a81159b6a90
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-12-26 01:22:52 +0900

    revert last approach

--- backend/controllers/auth_controller.js
+++ backend/controllers/auth_controller.js
@@ -19,7 +19,6 @@ exports.handle_login = [validateUserInput, async (req, res, next) => {
     try {
         const cookies = req.cookies;
         //console.log(`Cookie available at login: ${JSON.stringify(cookies)}`);
-        console.log(req.body);
         const errors = validationResult(req);
         if(!errors.isEmpty()) {
             res.status(400).send(errors.array());
