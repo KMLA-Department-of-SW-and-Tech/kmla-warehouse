e346dfb70ec8907bced5ffa415b915ce37e57e93
codeweb404 <codeweb.jcu@gmail.com>
2025-02-23 23:49:42 +0900

    implmented image changing for patch

--- backend/middleware/upload_image.js
+++ backend/middleware/upload_image.js
@@ -1,7 +1,9 @@
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const s3 = require('../config/s3'); // Import the S3 client from s3.js
+const { DeleteObjectCommand } = require("@aws-sdk/client-s3");
 require('dotenv').config(); // Load environment variables from .env
+const itemService = require("../services/item_service");
 
 const upload = multer({
   storage: multerS3({
@@ -33,7 +35,50 @@ const upload = multer({
 * Middleware to handle single image upload.
 * Adds imageUrl and imageKey to the request object upon success.
 */
-const handleImageUpload = (req, res, next) => {
+
+
+exports.deleteImage = async (fileKey) => {
+    if (!fileKey) {
+        console.warn("No imageKey provided for deletion.");
+        return { success: false, message: "No image found to delete." };
+    }
+
+    const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: fileKey,
+    };
+
+    try {
+        const command = new DeleteObjectCommand(params);
+        await s3.send(command); 
+        console.log(`File deleted successfully: ${fileKey}`);
+        return { success: true, message: "File deleted successfully" };
+    } catch (error) {
+        console.error(`Error deleting file: ${error.message}`);
+        throw new Error(`Could not delete file: ${error.message}`);
+    }
+};
+
+const handleImageUpload = async (req, res, next) => {
+    if(req.params.id) {
+        const id = req.params.id;
+        const item = await itemService.getOne(id);
+
+        if(!item) {
+            throw new Error("Item not found");
+        }
+
+        console.log(item);
+
+        try {
+            await exports.deleteImage(item.imageKey); 
+        } catch(e) {
+            throw(e);
+        }
+
+            
+    }
+
     const uploadSingle = upload.single('image');
     uploadSingle(req, res, async (err) => { 
         if (err instanceof multer.MulterError) {
