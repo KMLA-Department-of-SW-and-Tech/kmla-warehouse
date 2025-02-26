ed420298c9415f04cc66752fb382f0882ccd1e85
Yoo-Chan Jo <jo.yoochani@gmail.com>
2025-02-27 01:42:03 +0900

    Finished login modal

--- frontend/src/jsx/components/login-modal.jsx
+++ frontend/src/jsx/components/login-modal.jsx
@@ -25,7 +25,7 @@ export default function LoginModal({ openModal }) {
             <div className="modal-container">
                 <p>큼라창고에 존재하는 물품을 대여하거나 반납하기 위해서는 로그인이 되어 있어야 합니다. 아래 버튼을 통해 구글 계정으로 로그인을 진행한 후 사용해 주세요!</p>
                 <div className="google-login-btn0-container">
-                    <button className="google-login-btn">
+                    <button className="google-login-btn" onClick={handleGoogleLogin}>
                         <FcGoogle className="google-icon" />
                         <span>Log in with Google</span>
                     </button>
