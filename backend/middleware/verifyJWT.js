90d434cddc6b33583638c09430d2a0e186d71567
Yoo-Chan Jo <jo.yoochani@gmail.com>
2025-02-27 01:58:45 +0900

    Change dep

--- backend/middleware/verifyJWT.js
+++ backend/middleware/verifyJWT.js
@@ -1,20 +1,20 @@
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+// const jwt = require("jsonwebtoken");
+// require("dotenv").config();
 
-const verifyJWT = async (req, res, next) => {
-    const authHeader = req.headers.authorization || req.headers.Authorization;
-    if(!authHeader?.startsWith('Bearer')) return res.status(401).send("Invalid header request regarding authorization");
-    const token = authHeader.split(' ')[1];
-    jwt.verify(
-        token,
-        process.env.ACCESS_TOKEN_SECRET,
-        (err, decoded) => {
-            if(err) return res.status(403).send("Invalid access token"); // invalid token (includes access token expiration)
-            req.username = decoded.UserInfo.username;
-            req.roles = decoded.UserInfo.roles;
-            next();
-        }
-    );
-};
+// const verifyJWT = async (req, res, next) => {
+//     const authHeader = req.headers.authorization || req.headers.Authorization;
+//     if(!authHeader?.startsWith('Bearer')) return res.status(401).send("Invalid header request regarding authorization");
+//     const token = authHeader.split(' ')[1];
+//     jwt.verify(
+//         token,
+//         process.env.ACCESS_TOKEN_SECRET,
+//         (err, decoded) => {
+//             if(err) return res.status(403).send("Invalid access token"); // invalid token (includes access token expiration)
+//             req.username = decoded.UserInfo.username;
+//             req.roles = decoded.UserInfo.roles;
+//             next();
+//         }
+//     );
+// };
 
-module.exports = verifyJWT;
+// module.exports = verifyJWT;
