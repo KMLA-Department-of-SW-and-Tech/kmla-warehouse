7e00f8b4e26b02f981dca679a60cf94ed3ae8681
Yoo-Chan Jo <jo.yoochani@gmail.com>
2025-02-27 22:54:52 +0900

    Added error throwing to axiosPrivate

--- frontend/src/js/hooks/axiosPrivate.ts
+++ frontend/src/js/hooks/axiosPrivate.ts
@@ -10,54 +10,79 @@ const axiosPrivate: {
     delete: (apiUrl: string, accessToken: string, axiosConfig?: object) => Promise<any>
 } = {
     get: async (apiUrl, accessToken, axiosConfig) => {
-        const response = await axios.get(apiUrl, {
-            ...axiosConfig,
-            withCredentials: true,
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        });
-        return response;
+        try {
+            const response = await axios.get(apiUrl, {
+                ...axiosConfig,
+                withCredentials: true,
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+            return response;
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
     },
     post: async (apiUrl, requestData, accessToken, axiosConfig) => {
-        const response = await axios.post(apiUrl, requestData, {
-            ...axiosConfig,
-            withCredentials: true,
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        });
-        return response;
+        try {
+            const response = await axios.post(apiUrl, requestData, {
+                ...axiosConfig,
+                withCredentials: true,
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+            return response;
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
     },
     put: async (apiUrl, requestData, accessToken, axiosConfig) => {
-        const response = await axios.put(apiUrl, requestData, {
-            ...axiosConfig,
-            withCredentials: true,
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        });
-        return response;
+        try {
+            const response = await axios.put(apiUrl, requestData, {
+                ...axiosConfig,
+                withCredentials: true,
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+            return response;
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
     },
     patch: async (apiUrl, requestData, accessToken, axiosConfig) => {
-        const response = await axios.patch(apiUrl, requestData, {
-            ...axiosConfig,
-            withCredentials: true,
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        });
-        return response;
+        try {
+            const response = await axios.patch(apiUrl, requestData, {
+                ...axiosConfig,
+                withCredentials: true,
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+            return response;
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
     },
     delete: async (apiUrl, accessToken, axiosConfig) => {
-        const response = await axios.delete(apiUrl, {
-            ...axiosConfig,
-            withCredentials: true,
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        });
-        return response;
+        try {
+            const response = await axios.delete(apiUrl, {
+                ...axiosConfig,
+                withCredentials: true,
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+            return response;
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
     },
 };
 
