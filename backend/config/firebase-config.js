52959bcd0f639b88e72f40cdc4ed37e8dcf91402
Yoo-Chan Jo <jo.yoochani@gmail.com>
2025-02-27 09:08:57 +0900

    migrated things to dotenv

--- backend/config/firebase-config.js
+++ backend/config/firebase-config.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("path/to/serviceAccountKey.json");
+const serviceAccount = require("./serviceAccount").serviceAccount;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
