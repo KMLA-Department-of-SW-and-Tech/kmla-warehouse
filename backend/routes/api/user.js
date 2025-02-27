2e60b0f24b87576bcfd19b3dde12b59ac47904ea
Yoo-Chan Jo <jo.yoochani@gmail.com>
2025-02-27 23:54:08 +0900

    import change

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const verifyJWT = require("../../middleware/verifyJWT");
-const verifyRoles = require("../../middleware/verifyUserRoles"); // in case of admin secrurity
+const verifyRoles = require("../../middleware/verifyRoles"); // in case of admin secrurity
 const  userController = require("../../controllers/user_controller");
 
 router.route("/test")
