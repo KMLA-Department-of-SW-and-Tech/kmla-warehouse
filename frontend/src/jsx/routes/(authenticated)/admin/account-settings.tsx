d7676a9c0f333c81c7b8bf5f7d4c8d2da920ac78
hyk03031 <yha525320@gmail.com>
2025-02-28 16:34:17 +0900

    admin account setting page to logout page

--- frontend/src/jsx/routes/(authenticated)/admin/account-settings.tsx
+++ frontend/src/jsx/routes/(authenticated)/admin/account-settings.tsx
@@ -10,27 +10,7 @@ const { Title } = Typography;
 
 const AdminSettingPage = () => {
   const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(''); // State to hold error messages
   const navigate = useNavigate();
-  
-  // Function to handle password change
-  const handlePasswordChange = async (values) => {
-    setLoading(true);
-    setErrorMessage(""); // Reset error message on new attempt
-    try {
-      // await authService.changePassword(values.currentPassword, values.newPassword);
-      message.success('Password changed successfully!');
-    } catch (error) {
-      console.log(error);
-      // If an error occurs, display the error message and prevent the success message from showing
-      const responseMessage = error.response?.data || 'Failed to change password. Please try again.';
-      setErrorMessage(responseMessage); // Set error message for display in the form
-      message.error('Failed to change password.'); // Display error notification
-      throw(error);
-    } finally {
-      setLoading(false);
-    }
-  };
 
   // Function to handle logout
   const handleLogout = async () => {
