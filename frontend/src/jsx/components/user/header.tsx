72a17e3581fff87d447f664216f4f85acd6c4bcf
Yoo-Chan Jo <jo.yoochani@gmail.com>
2025-02-28 16:12:54 +0900

    user header fix

--- frontend/src/jsx/components/user/header.tsx
+++ frontend/src/jsx/components/user/header.tsx
@@ -1,28 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // 페이지 이동을 위한 훅
 import './header.css';
+import { useAuth } from '../../contexts/authContext';
 // import authService from '../../api/authService';
 
 // Header 컴포넌트 정의
 const Headbar: React.FC = () => {
-  const [loading, setLoading] = useState<Boolean>(true);
-  const [currentUserName, setCurrentUserName] = useState<string | null>(null);
+  // const [loading, setLoading] = useState<Boolean>(true);
+  // const [currentUserName, setCurrentUserName] = useState<string | null>(null);
   const navigate = useNavigate(); // 페이지 이동을 위한 navigate 함수 생성
 
-  useEffect(() => {
-    const fetchCurrentUser = async () => {
-      try {
-        const data = await authService.currentUser();
-        setCurrentUserName(data);
-      } catch (error) {
-        //console.error('Error fetching user info:', error);
-        setCurrentUserName(null); // 에러 발생 시 로그인 상태를 비로그인으로 처리
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchCurrentUser();
-  }, []);
+  const authValue = useAuth();
+
+  // useEffect(() => {
+  //   const fetchCurrentUser = async () => {
+  //     try {
+  //       const data = await authService.currentUser();
+  //       setCurrentUserName(data);
+  //     } catch (error) {
+  //       //console.error('Error fetching user info:', error);
+  //       setCurrentUserName(null); // 에러 발생 시 로그인 상태를 비로그인으로 처리
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+  //   fetchCurrentUser();
+  // }, []);
 
   const handleLogoClick = () => {
     navigate('/home'); // 로고 클릭 시 홈 화면으로 이동
@@ -39,8 +42,9 @@ const Headbar: React.FC = () => {
           KMLA Warehouse
         </div>
         <div className="user-info" onClick={handleHelloClick} style={{cursor: 'pointer'}}>
-          {loading ? <span></span> : currentUserName ? (
-            <span>{currentUserName}님, KMLA WAREHOUSE에 오신 것을 환영합니다</span>
+          {/* <span>{currentUserName}님, KMLA WAREHOUSE에 오신 것을 환영합니다</span> */}
+          {authValue.userLoggedIn ? (
+            <span>로그인 됨 ㅋ</span>
           ) : (
             <span>로그인을 하신 후 다른 기능을 사용하실 수 있습니다</span>
           )}
