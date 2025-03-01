0549682c23560b4121bcfa1d399a7341b1b44f6f
Yoo-Chan Jo <jo.yoochani@gmail.com>
2025-03-02 00:23:44 +0900

    Blocked double import

--- frontend/src/jsx/routes/(authenticated)/admin/account-settings.tsx
+++ frontend/src/jsx/routes/(authenticated)/admin/account-settings.tsx
@@ -4,7 +4,6 @@ import { signUserOut } from '../../../../js/firebase/auth';
 import Sidebar from '../../../components/admin/admin-sidebar';
 import Headbar from '../../../components/admin/admin-header'; // Assuming you have a header component like in the EquipmentListPage
 import { useNavigate } from 'react-router-dom';
-import { signUserOut } from "../../../../js/firebase/auth";
 
 const { Sider, Content } = Layout;
 const { Title } = Typography;
