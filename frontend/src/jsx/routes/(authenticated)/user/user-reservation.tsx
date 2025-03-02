56736bd7587a5489673ba271c46c27562b6f1565
Yean Lee <talk2yeanlee@gmail.com>
2025-03-02 11:33:33 +0900

    reservation page

--- frontend/src/jsx/routes/(authenticated)/user/user-reservation.tsx
+++ frontend/src/jsx/routes/(authenticated)/user/user-reservation.tsx
@@ -1,24 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { Typography, Card, Row, Col, Spin, Layout, message, Button } from 'antd';
-import Sidebar from '../../../components/sidebar/user-sidebar.tsx';
-import { itemService } from "../../../../js/api/itemService.ts";
-import Headbar from "../../../components/header/header.tsx";
-import { UnorderedListOutlined } from '@ant-design/icons';
-
-import { GetLog } from "../../../../js/types/Log.ts";
-import { GetItem } from "../../../../js/types/Item.ts";
+import React, { useState, useEffect } from 'react';
+import { Typography, Card, Row, Col, Spin, Layout, Input } from 'antd';
+import { UnorderedListOutlined } from '@ant-design/icons'; 
+import { useNavigate } from 'react-router-dom';
+import Sidebar from '../../../components/sidebar/user-sidebar';
+import { itemService } from '../../../../js/api/itemService'; 
+import Headbar from '../../../components/header/header';
+import { GetItem } from '../../../../js/types/Item';
+import { GetLog } from '../../../../js/types/Log';
 import { useAuth } from "../../../contexts/authContext/index.jsx";
-import './user-reservation.css';
+import "./user-reservation.css";
 
 const { Sider, Content } = Layout;
 const { Title } = Typography;
+const { Search } = Input;
 
 export default function ReservationStatus() {
   const [loading, setLoading] = useState(true);
-  const [currentUserId, setCurrentUserId] = useState("");
-  
   const [reservationList, setReservationList] = useState<(GetLog & { item?: GetItem })[]>([]);
-
+  const [filteredReservationList, setFilteredReservationList] = useState<(GetLog & { item?: GetItem })[]>([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth); 
+  const navigate = useNavigate();
   const authValue = useAuth();
 
   useEffect(() => {
@@ -26,96 +28,125 @@ export default function ReservationStatus() {
       try {
         const username = authValue?.user?.username;
         if (!username) return;
-        setCurrentUserId(username);
 
         const reservations = await itemService.getReservations(username, authValue.accessToken);
         setReservationList(reservations);
       } catch (error) {
-        console.error("Failed to fetch:", error);
-        message.error("데이터를 불러오는 데 실패했습니다.");
+        console.error('Failed to fetch:', error);
       } finally {
         setLoading(false);
       }
     };
 
     fetchReservationAndEquipment();
+
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
   }, [authValue.accessToken]);
+
+  useEffect(() => {
+    if (searchQuery) {
+      setFilteredReservationList(
+        reservationList.filter(reservation =>
+          reservation.item?.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          reservation.item?.location.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      );
+    } else {
+      setFilteredReservationList(reservationList);
+    }
+  }, [searchQuery, reservationList]);
+
+  const handleViewDetails = (equipmentId: string) => {
+    navigate(`/item/${equipmentId}`);
+  };
 
   const handleReturn = async (reservationId: string) => {
     try {
       await itemService.returnItem(reservationId, authValue.accessToken);
-      message.success('반납 요청이 성공적으로 처리되었습니다.');
       setReservationList(prevList => prevList.filter(r => r._id !== reservationId));
     } catch (error) {
       console.error('Failed to return item:', error);
-      message.error('반납 요청에 실패했습니다. 다시 시도해 주세요.');
     }
   };
 
   return (
-    <Layout>
+    <Layout className="reservation-layout">
       <Headbar />
-      <Sider>
-        <Sidebar />
-      </Sider>
+      
+      {windowWidth > 768 && (
+        <Sider className="sidebar">
+          <Sidebar />
+        </Sider>
+      )}
 
-      <Layout className="equipment-layout">
-        <Content className="equipment-content">
-          <Title level={2} className="equipment-title">
-            <UnorderedListOutlined />
-            예약현황 보기
-          </Title>
+      <Layout style={{ marginLeft: windowWidth > 768 ? 250 : 0 }}>
+        <Content className="reservation-content">
+          <div className="reservation-card-container">
+            {/* Mobile Device Warning */}
+            {windowWidth <= 768 && (
+              <div className="mobile-warning">
+                <Typography.Text type="warning" style={{ fontSize: '16px' }}>
+                  노트북을 이용하는 것을 권장드립니다.
+                </Typography.Text>
+              </div>
+            )}
 
-          {loading ? (
-            <Spin size="large" />
-          ) : (
-            <Row gutter={[16, 16]} className="equipment-row">
-              {reservationList.length > 0 ? (
-                reservationList.map((reservation) => (
-                  <Col xs={24} sm={12} md={8} lg={4} key={reservation._id || Math.random()}>
-                    <Card
-                      hoverable
-                      cover={
-                        <div className="equipment-image-container">
-                          {reservation.item?.imageUrl ? (
-                            <img
-                              src={reservation.item.imageUrl}
-                              alt={reservation.item.name}
-                              className="equipment-image"
-                            />
-                          ) : (
-                            <Typography.Text className="noImageText">이미지 없음</Typography.Text>
-                          )}
-                        </div>
-                      }
-                      className="equipment-card"
-                    >
-                      <Card.Meta
-                        title={reservation.item?.name || "이름 없음"}
-                        description={
-                          <>
-                            <span>{reservation.quantity}</span>
-                            <span> / {reservation.item?.location || "위치 정보 없음"} / {reservation.timestamp ? new Date(reservation.timestamp).toLocaleDateString() : "날짜 없음"}</span>
-                          </>
+            <Title level={2} className="reservation-title">
+              <UnorderedListOutlined className="reservation-icon" />
+              예약 현황 보기
+            </Title>
+
+            {loading ? (
+              <Spin size="large" />
+            ) : (
+              <Row gutter={[16, 16]} className="reservation-row">
+                {filteredReservationList.length > 0 ? (
+                  filteredReservationList.map((reservation) => (
+                    <Col xs={24} sm={12} md={8} lg={6} key={reservation._id}>
+                      <Card
+                        hoverable
+                        cover={
+                          <div className="reservation-image-container">
+                            {reservation.item?.imageUrl ? (
+                              <img src={reservation.item.imageUrl} alt={reservation.item.name} className="reservation-image" />
+                            ) : (
+                              <Typography.Text className="noImageText">이미지 없음</Typography.Text>
+                            )}
+                          </div>
                         }
-                        className="equipment-meta"
-                      />
-
-                      <Button
-                        type="primary"
-                        onClick={() => handleReturn(reservation._id)}
-                        className="returnButton"
+                        className="reservation-card"
+                        onClick={() => handleViewDetails(reservation.item?._id || '')}
                       >
-                        반납하기
-                      </Button>
-                    </Card>
-                  </Col>
-                ))
-              ) : (
-                <Typography.Text>데이터가 없습니다.</Typography.Text>
-              )}
-            </Row>
-          )}
+                        <Card.Meta
+                          title={reservation.item?.name || '이름 없음'}
+                          description={
+                            <>
+                              <span>{reservation.quantity}</span>
+                              <span> / {reservation.item?.location || '위치 정보 없음'} / {reservation.timestamp ? new Date(reservation.timestamp).toLocaleDateString() : '날짜 없음'}</span>
+                            </>
+                          }
+                          className="reservation-meta"
+                        />
+
+                        <button
+                          type="button"
+                          onClick={() => handleReturn(reservation._id)}
+                          className="returnButton"
+                        >
+                          반납하기
+                        </button>
+                      </Card>
+                    </Col>
+                  ))
+                ) : (
+                  <Typography.Text>데이터가 없습니다.</Typography.Text>
+                )}
+              </Row>
+            )}
+          </div>
         </Content>
       </Layout>
     </Layout>
