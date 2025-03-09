0e0fb2ef8297d3d19a6125d11c44cbb3d0cb9725
Yoo-Chan Jo <jo.yoochani@gmail.com>
2025-03-09 20:08:32 +0900

    Changed vite config

--- backend/app.js
+++ backend/app.js
@@ -44,7 +44,7 @@ app.use(
 );
 
 // API routes
-app.use("/api", apiRouter);
+app.use("/", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig(({command, mode}) => {
+export default defineConfig(({ mode }) => {
     const isDev = mode === "development";
     
     return {
         plugins: [react()],
         server: {
             proxy: {
-                "/api": isDev ? "http://localhost:3000" : "https://kmla-warehouse-backend.netlify.app",
+                "/api": {
+                    target: isDev ? "http://localhost:3000" : "https://kmla-warehouse-backend.netlify.app",
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ''),
+                    secure: false,
+                    ws: true,
+                }
             },
         },
     }
