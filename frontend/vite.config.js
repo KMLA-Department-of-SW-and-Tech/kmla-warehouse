b1b81fe28f28b1146dec2908d996c44b896771b7
Yoo-Chan Jo <jo.yoochani@gmail.com>
2025-03-09 21:58:26 +0900

    Added netlify.toml in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         server: {
             proxy: {
                 "/api": {
-                    target: "https://kmla-warehouse-backend.netlify.app",//isDev ? "http://localhost:3000" : "https://kmla-warehouse-backend.netlify.app",
+                    target: "http://localhost:3000",
                     changeOrigin: true,
                     secure: false,
                     ws: true,
